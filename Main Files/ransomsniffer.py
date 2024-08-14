b2cdac6d11ea1ed7245c59aaf10e7956691fd460
SecOpsWizard <42916447+phantom0004@users.noreply.github.com>
2024-08-14 18:17:50 +0200

    Update and rename ransomsniffer.py to Main Files/ransomsniffer.py

---
+++ Main Files/ransomsniffer.py
@@ -0,0 +1,6 @@
+# This program is designed to detect potential ransomware files by leveraging advanced statistical analysis on executable files. 
+# It identifies common ransomware techniques and alerts the user if a file is deemed unsafe. 
+# The program is compatible with various ransomware samples, including _KRYPT0S and WannaCry.
+# Intended for educational purposes, this tool aims to enhance understanding and defense against ransomware attacks.
+
+print("Currently Still Under Development ...")
--- ransomsniffer.py
+++
@@ -1,4 +0,0 @@
-# This program will aim to detect ransomware files. By utalizing sophisticated statis analysis on executable files, this program will be able to detect ransomware common techniques and also inform the user if such a file is deemed unsanfe. This will also work with _KRYPT0S as well as other files like Wannacry.
-# This is aimed to be used for educational purposes, further defending against such attack trends
-
-print("In progress . . .")
