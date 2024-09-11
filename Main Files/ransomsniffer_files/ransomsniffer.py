fb53019551d9aba34da2dd111762400fb13d4410
SecOpsWizard <42916447+phantom0004@users.noreply.github.com>
2024-09-11 11:05:26 +0200

    Enhanced error handling and better code structure

--- Main Files/ransomsniffer_files/ransomsniffer.py
+++ Main Files/ransomsniffer_files/ransomsniffer.py
@@ -7,10 +7,11 @@ try:
 except ModuleNotFoundError:
     exit("Custom modules not found. Please ensure you have the 'yara_rules.py', 'pe_analysis.py' and 'virus_total.py'!")
 try:
-    import yara
+    import yara 
     import pefile
+    from termcolor import colored
 except ModuleNotFoundError:
-    exit("Yara and/or pefile not found! Please ensure you download all dependancies from the 'requirements.txt' file")
+    exit("Missing Dependancies! Please ensure you download all dependancies from the 'requirements.txt' file")
 
 # Program Intro Banner
 def intro_banner():
@@ -63,52 +64,59 @@ def menu_switch(choice):
     scan_banner()
 
 # Start virus total scan using module
-def virus_total_scan():    
+def virus_total_scan():
+    # Get user arguments needed for API
     API_KEY = input("Enter your VirusTotal API key > ").strip()
     data, choice = virus_total_user_arguments()
 
     # Create the VirusTotalAPI object without client_obj initially
     virus_total_object = virus_total.VirusTotalAPI(choice, data, API_KEY)
     # Connect to VirusTotal API and get the client_object
-    client_object, _ = virus_total_object.connect_to_endpoint()
+    client_object, status_message = virus_total_object.connect_to_endpoint()
+    if status_message == "api_fail":
+        exit("API Error: The 'vt' library encountered an issue. Please ensure your API key is valid. If the problem persists, try re-installing the 'vt' library.")
+    elif status_message == "general_fail":
+        exit("General Error: A failure occurred while connecting to the API. Please retry, and if the issue continues, consider reporting it on GitHub.")
+    
     # Set the client_obj attribute within the same object
     virus_total_object.client_obj = client_object
-    
-    api_request_string = virus_total_object.craft_api_request()
-    output, function_status = virus_total_object.send_api_request_using_vt(api_request_string)
 
-    # Parse output, and see if it was a success
+    # Craft the API request string
+    api_request_string = virus_total_object.craft_api_request()
+    
+    # Send the API request and get status
+    output, function_status = virus_total_object.send_api_request_using_vt(api_request_string)
     if function_status == "api_fail":
-        print(virus_total_object.parse_API_error(output))
+        exit(virus_total_object.parse_API_error(output))
     elif function_status == "general_fail":
-        print(output)
-    else:
-        virus_total_object.parse_API_output(output)
+        exit(output)
+    
+    # Get the API response and parse the virus total output for file/url
+    virus_total_object.parse_API_output(output)
 
 # Handle user input for the virus total API
 def virus_total_user_arguments():
     print("\nPlease select what you wish to scan :")
     print("[1] Scan a file \n[2] Scan a URL")
     user_choice = input("Choice > ").strip()
-    print()
     
     data = ""
     if user_choice == "1":
-        data = input("Enter a file hash or file path to scan > ")
-        if "/" in data or "\\" in data or "." in data:
+        data = input("\nEnter a file hash or file path to scan > ")
+        if any(keyword in data for keyword in ["\\", "http", "https"]):
             hash_algo = input("Enter the hashing algorithm to use [md5, sha1, sha256] (Leave blank for sha256) > ").strip().lower()
             data = hash_file(data, hash_algo)
-            if not data: 
-                exit("[-] File does not exist in system! Please ensure the path is valid")
-            else:
-                print(f"[+] Successfully hashed file using {hash_algo}. Scanning -> {data}")
+            
+            parse_hash_output(data) # Identifies any hash errors in runtime
+            print(colored(f"✔ Successfully hashed file -> {data}", "green"))
     elif user_choice == "2":
-        data = input("Enter the URL you wish to scan > ")
+        data = input("\nEnter the URL you wish to scan > ").strip()
     else:
         exit("[-] Invalid Input! Please enter a value between 1 and 2")
 
-    print("\t\t   ------- \n")
+    print(f"{"-"*100}\n")
     user_choice = "files" if user_choice == "1" else "urls"
+    
     return data, user_choice
 
 # Hash file for virus total scan
@@ -126,6 +134,18 @@ def hash_file(path, hash_algo="sha256"):
     
     return virus_total_object.hash_file(file_data, hash_algo)
 
+def parse_hash_output(output):
+    message = None
+
+    if not output:
+        message = "[-] An unknown error occurred: no object returned from the hashing method."
+    elif output == "hashing_error":
+        message = "[-] Hashing error detected. Please ensure the data is valid or try using different data."
+    elif output == "hash_digest_error":
+        message = "[-] Hash digest error: the hash object was created, but the final output could not be parsed."
+
+    if message: exit(message)
+    
 # Display virus total help menu
 def display_API_help():
     print("""
@@ -147,19 +167,17 @@ More info: https://virustotal.readme.io/docs/please-give-me-an-api-key
 
 # Enter yara scan menu (still need to implement)
 def default_yara_scan():
-    pass
+    exit("Still in development . . .")
 
 # Handle menu user option
 def handle_user_arguments():
     try:
         usr_input = input("Choice > ")
+        if usr_input != "3": menu_switch(usr_input)
     except KeyboardInterrupt:
         exit("[!] Program Exited Successfully")
     if not usr_input or usr_input not in ["1", "2", "3"]:
-        exit("Invalid Choice Input - Please ensure your input is in the range of 1-3!")
-    
-    if usr_input != "3":
-        menu_switch(usr_input)
+        exit("[-] Invalid Choice Input - Please ensure your input is in the range of 1-3!")
         
     if usr_input == "1":
         virus_total_scan()
